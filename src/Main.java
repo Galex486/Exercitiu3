ea2feb6972f377a193cefeedec3345e2616f6229
alexandru.grama <galex486@gmail.com>
2023-10-06 15:30:18 +0300

    modif

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ public class Main {
     public static void main (String[] args) {
 
         float S = 0;
-        for(int i = 1;i < 100;i++){
-            S += (float) i /(i+2);
+        float T = 0;
+        for(int i = 1;i < 99;i++){
+            S = (float) i /(i+2);
+            T += (float) i /(i+2);
             int D = i + 2;
             System.out.print(i + "/" + D);
             ++i;
 
-            System.out.println("; Divide = " + S+"\n");
+            System.out.println("; Divide = " + S);
         }
-        System.out.println(" Sum = " + S+"\n");
+
+        System.out.println("SumTotal = " + T);
     }
 }
